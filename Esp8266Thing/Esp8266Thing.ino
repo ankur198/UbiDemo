3c260bb43be86dd0f093ce8dac1b20ae72df3bc4
Ankur Nigam <ankur.nigam198@gmail.com>
2018-06-23 22:19:08 +0530

    pin control with console test app

--- Esp8266Thing/Esp8266Thing.ino
+++ Esp8266Thing/Esp8266Thing.ino
@@ -5,6 +5,10 @@ char *pwd = "youCANhack1";
 
 WiFiServer server(80);
 
+int pins[] = { 16,5,4,0,2,14,12,13,15 };
+int vals[] = { 0,0,0,0,0,0,0,0,0 };
+
+
 void ConnectToWifi()
 {
 	Serial.println("Attemting to connect");
@@ -27,33 +31,127 @@ void StartServer()
 	Serial.println("Server started");
 }
 
+void InitializePin()
+{
+	for (uint i = 0; i < 9; i++)
+	{
+		pinMode(pins[i], OUTPUT);
+	}
+}
+
 void setup()
 {
 	Serial.begin(9600);
 	ConnectToWifi();
 	StartServer();
+	InitializePin();
 }
 
-void SendResponse(WiFiClient client, String response)
+
+void SetPinVal(int pin, int pinValue)
 {
-	String s = response;
-	client.print(s[]);
-	Serial.println("Client disconnected");
+	Serial.println(String(pin) + ":" + String(pinValue));
+	for (int i = 0; i < 9; i++)
+	{
+		if (pins[i] == pin)
+		{
+			vals[i] = pinValue;
+			analogWrite(pin, pinValue);
+			return;
+		}
+	}
+	Serial.println("no pin found");
+}
+
+String SetPin(String req)
+{
+	String raw = req.substring(req.indexOf('/') + 1);
+	raw = raw.substring(raw.indexOf('/') + 1);
+	Serial.println(raw);
+	String pin = String(raw[0]);
+	if (raw[1] != '/')
+	{
+		pin += String(raw[1]);
+		raw = raw.substring(3);
+	}
+	else raw = raw.substring(2);
+	String pinValue = String();
+	for (uint i = 0; i < raw.indexOf('H') - 1; i++)
+	{
+		pinValue += String(raw[i]);
+	}
+	Serial.println(raw);
+	Serial.println(pin.toInt());
+	Serial.println(pinValue.toInt());
+	SetPinVal(pin.toInt(), pinValue.toInt());
+	return "OK";
+
+}
+
+int GetVal(int pin)
+{
+	for (int i = 0; i < 9; i++)
+	{
+		if (pin == pins[i])
+		{
+			return vals[i];
+		}
+	}
+	return 0;
+}
+
+String GetPin(String req)
+{
+	String raw = req.substring(req.indexOf('/') + 1);
+	raw = raw.substring(raw.indexOf('/') + 1);
+	String pin = String(raw[0]);
+	if (raw[1] != '/')
+	{
+		pin += String(raw[1]);
+	}
+	Serial.println(raw);
+	Serial.println(pin.toInt());
+	int val = GetVal(pin.toInt());
+	return String(val);
+}
+
+String ProcessResponse(String req)
+{
+	if (req.indexOf("setpin") != -1)
+	{
+		Serial.println("Going to set pin");
+		return SetPin(req);
+	}
+	else if (req.indexOf("getpin") != -1)
+	{
+		Serial.println("Going to get pin");
+		return GetPin(req);
+	}
 }
 
 void GotClient(WiFiClient client)
 {
+	int timeout = 0;
 	//wait to send data
 	Serial.println("new client");
 	while (!client.available())
 	{
 		delay(1);
+		timeout++;
+		if (timeout==300)
+		{
+			return;
+		}
 	}
 	//started recieving data
 	String req = client.readStringUntil('\r');
 	Serial.println(req);
 	client.flush();
-	SendResponse(client, req);
+	req = ProcessResponse(req);
+	Serial.println(req);
+	String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n" + req;
+	client.print(s);
+	Serial.println("Client disconnected");
 }
 
 
@@ -66,10 +164,12 @@ void CheckForClient()
 		//no client
 	}
 	GotClient(client);
+
 }
 
 
 void loop()
 {
 	CheckForClient();
+	delay(20);
 }
---
+++ esp8266Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esp8266Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            set();
+            get();
+            Console.ReadLine();
+        }
+
+        async static void set()
+        {
+            HttpClient client = new HttpClient();
+            
+            client.BaseAddress = new System.Uri("http://192.168.1.104/");
+            var x = await client.GetStringAsync("/setpin/0/103");
+            Console.WriteLine(x);
+        }
+        async static void get()
+        {
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new System.Uri("http://192.168.1.104/");
+            var x = await client.GetStringAsync("/getpin/0");
+            Console.WriteLine(x);
+        }
+    }
+}
